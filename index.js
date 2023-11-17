b24cc30de1a741e81fc5a0a2b2abc49819b023a5
William <coe9257@gmail.com>
2023-11-16 17:45:18 -0800

    Finished Without Animations

--- index.js
+++ index.js
@@ -1,26 +1,71 @@
-function create_elements(type, attributes) {
+const book_array = [];
+
+let id_num = 0;
+
+function create_elements(type, attributes, class_name, id) {
     const element = document.createElement(type);
     element.setAttribute('style', `${attributes}`);
+
+    if (class_name != undefined) {
+        element.classList.add(`${class_name}`);
+    };
+
+    let does_id_already_exist = false
+
+    // checks if ID parameter is already in an element in book_array
+    book_array.forEach(element => {
+        if (element.id === id) {
+            does_id_already_exist = true;
+        }else {
+            id = `DUPLICATE_${id_num}`;
+            id_num++;
+        };
+    });
+
+    if (id != undefined && does_id_already_exist == false) {
+        element.id = `${id}`;
+    };
+
+    
     return element;
-}
+};
+
+function testString(book_title, book_author, book_pages) {
+    if (book_title != "" && book_title != null 
+        && book_author != "" && book_author != null 
+        && book_pages != "" && book_pages != null) {
+        return true;
+    }else {
+        return false;
+    }
+};
 
 function add_book() {
 
     function add_book_entry_box() {
-        const input_color = 'powderblue'
+        
         const body = document.querySelector('body');
 
-        const temporary_container = document.createElement('div');
+        if (!document.querySelector('.temporary_container')) {
+            const temporary_container = document.createElement('div');
             temporary_container.setAttribute("style", `width: 100%; padding-top: 5rem; 
                 padding-bottom: 5rem; background-color: white; display: flex; justify-content: center;
                 align-items: center;`);
                 temporary_container.classList.add("temporary_container");
         
-        const book_display = document.getElementById(`book_display`);
+            const book_display = document.getElementById(`book_display`);
 
-        body.insertBefore(temporary_container, book_display);
+            body.insertBefore(temporary_container, book_display);
+        };
 
+
+    }
+    add_book_entry_box();
+
+    function create_div_parts(){
+        const temporary_container = document.querySelector('.temporary_container');
         const display_box = document.createElement('div');
+        const input_color = 'powderblue';
             display_box.setAttribute('style', `height: 30rem; width: 25rem; border: 5px solid black;
                 background-color: midnightblue; display: flex; flex-direction: column; 
                 align-items: flex-start; padding-top: 2.5rem;`);
@@ -38,7 +83,7 @@ function add_book() {
             book_title_flex_container.appendChild(book_title_div);
 
             const book_title_input = create_elements('input', `width: 90%; height: 1.5rem; 
-                background-color: ${input_color}; margin: 0 auto;`);
+                background-color: ${input_color}; margin: 0 auto;`, `book_title_input`);
             book_title_flex_container.appendChild(book_title_input);
 
         display_box.appendChild(book_title_flex_container);
@@ -54,7 +99,7 @@ function add_book() {
             book_author_flex_container.appendChild(book_author_div);
 
             const book_author_input = create_elements('input', `width: 90%; height: 1.5rem; 
-                background-color: ${input_color}; margin: 0 auto;`);
+                background-color: ${input_color}; margin: 0 auto;`, `book_author_input`);
             book_author_flex_container.appendChild(book_author_input);
 
         display_box.appendChild(book_author_flex_container);        
@@ -70,7 +115,7 @@ function add_book() {
             book_pages_flex_container.appendChild(book_pages_div);
 
             const book_pages_input = create_elements('input', `width: 90%; height: 1.5rem; 
-                background-color: ${input_color}; margin: 0 auto;`);
+                background-color: ${input_color}; margin: 0 auto;`, `book_pages_input`);
             book_pages_flex_container.appendChild(book_pages_input);
 
         display_box.appendChild(book_pages_flex_container);  
@@ -82,26 +127,108 @@ function add_book() {
                 button.classList.add("add_book_submit_button");
 
             display_box.appendChild(button);
-
-        function submit_book() {
-            
-        }
-
-            document.querySelector('.add_book_submit_button').addEventListener("click", submit_book);
     }
-    add_book_entry_box()
-}   
+    create_div_parts();
+
+    function submit_book() {
+
+        const book_title = document.querySelector(`.book_title_input`).value;
+        const book_author = document.querySelector(`.book_author_input`).value;
+        const book_pages = document.querySelector(`.book_pages_input`).value;
+
+        if (testString(book_title, book_author, book_pages) == true) {
+            submit_book_values(book_title, book_author, book_pages);
+        }else {
+            return false;
+        };
+    };
+
+    document.querySelector('.add_book_submit_button').addEventListener("click", submit_book);
+    
+};
+
+function submit_book_values(book_title, book_author, book_pages) {
+    let text_attributes = `width: 90%; height: 1rem; border: 1px solid black; 
+        background-color: white; color: black; display: flex; flex-direction: column; 
+        margin: 0 auto; margin-top: 1.5rem; padding: .5rem;`
+        // type, attributes, class_name, id
+    const book_element = create_elements('div', `width: 17.5rem; height: 17.5rem; border: 1px solid black;
+        background-color: black; margin-top: 2.5rem; book_object; margin: 0 0 0 2rem; display: flex;
+        flex-direction: column; align-items: center book_element; margin-top: 2rem`, 'book_input', `xr_${id_num}`);
+        id_num++;
+
+    const book_title_div = create_elements(`div`, `${text_attributes} margin-top: 2.5rem;`, `book_objects`, 
+        `first_book_object_div_${id_num}`);
+        id_num++;
+        book_title_div.textContent = book_title;
+    
+    book_element.appendChild(book_title_div);
+
+    const book_author_div = create_elements(`div`, `${text_attributes}`, `book_objects`, `${book_author}${id_num}`);
+            book_author_div.textContent = book_author;
+            id_num++;
+
+    book_element.appendChild(book_author_div);
+
+        const book_pages_div = create_elements(`div`, `${text_attributes}`, `book_objects`, `${book_pages}${id_num}`);
+            book_pages_div.textContent = `Pages: ${book_pages}`;
+            id_num++;
+
+    book_element.appendChild(book_pages_div);
+
+    // create CSS for close button
+    const delete_book_div = create_elements('button', `${text_attributes} color: white; background-color: teal; display: block;
+        height: auto; padding: 1rem; border: 5px solid ridge;`);
+        delete_book_div.textContent = 'DELETE';
+        delete_book_div.addEventListener('click', deleteBook);
+
+    book_element.appendChild(delete_book_div);
+
+
+    book_array.push(book_element);
+
+    append_books_from_array();
+};
+
+function append_books_from_array() {
+    book_array.forEach((element) => {
+        const book_display_box = document.getElementById("book_display");
+        book_display_box.appendChild(element);
+    });
+    close_add_book_container();
+};
+
+function close_add_book_container() {
+
+    const parent_container = document.querySelector('.temporary_container');
+    const submit_book_div = document.querySelector('.display_box');
+        parent_container.removeChild(submit_book_div);
+
+    const body = document.querySelector('body');
+    const temporary_container = document.querySelector('.temporary_container');
+        body.removeChild(temporary_container);
+};
+
+function deleteBook(e) {
+    //button on book div
+    const clickedElement = this;
+    //book container div
+    const parentElement = clickedElement.parentNode;
+    //container holding all book divs
+    const div_container = parentElement.parentNode;
+
+    const book_div_id = parentElement.id
+    
+    let array_id = null;
+
+    book_array.forEach((element, index) => {
+        if (element.id == book_div_id) {
+            book_array.splice(index, 1);
+            div_container.removeChild(parentElement);
+        };
+    })
+
+    // console.log(parentElement);
+};
 
 document.getElementById("add_book").addEventListener("click", add_book);
-
-
-
-    // function add_single_book() {
-    //     const add_book_button = document.querySelector("#add_book");
-    //     const container = document.createElement('div');
-    //         container.setAttribute("style", `height: 17.5rem; width: 12.5rem; border: 5px solid black; 
-    //         background-color: midnightblue; margin: 5rem;`);
-    //     const book_container = document.getElementById("book_display");
-    //     book_container.appendChild(container);
-    // }
-
